3e95826d4aa7b87aa4d62e82d4f6cf4cb811d01a
josaguil <josaguil@vector.com.mx>
2025-03-13 07:53:38 -0600

    trabajando rendimientos bmks

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import yfinance as yf
+from datetime import datetime
+
+fecha = datetime.today()
+fecha = datetime(year=fecha.year, month=fecha.month, day=fecha.day)
 
 bmks_rv = [
     "^MXX", "^SPESG"
 ]
-df = yf.download(bmks_rv, start='2020-01-01')
+df = yf.download(bmks_rv, start=datetime(year=fecha.year - 1, month=1, day=1).strftime("%Y-%m-%d"))
 
 st.write(df)
