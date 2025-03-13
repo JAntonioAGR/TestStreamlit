f9439cb97173456ef2f5b007b0629b2cac29be26
josaguil <josaguil@vector.com.mx>
2025-03-13 09:21:47 -0600

    trabajando rendimientos bmks

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ fecha = datetime.today()
 fecha = datetime(year=fecha.year, month=fecha.month, day=fecha.day)
 
 bmks_rv = [
-    "^MXX", "^SPESG", "^SPGSCITR"
+    "^MXX", "^SPESG", "^SPGSCI"
 ]
 df = yf.download(bmks_rv, start=datetime(year=fecha.year - 1, month=1, day=1).strftime("%Y-%m-%d"))
 
