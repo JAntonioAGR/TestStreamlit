3d165d5a5324fdcb614954ff7b156adb0acddaae
josaguil <josaguil@vector.com.mx>
2025-03-28 10:23:51 -0600

    trabajando en grafica rendimientos

--- test.py
+++ test.py
@@ -519,7 +519,7 @@ fondo = st.selectbox(
 #st.write(precios_fondos_df[fondo])
 
 precios_fondo_bmk_df = pd.merge(precios_fondos_df[fondo].reset_index(), precios_bmks_df[fondo2benchmark[fondo]["Benchmarks"]].reset_index(), on="Fecha", how="left")
-precios_fondo_bmk_df.set_index("Fecha", ipnlace=True)
+precios_fondo_bmk_df.set_index("Fecha", inplace=True)
 st.write(precios_fondo_bmk_df)
 
 st.write("ok")
