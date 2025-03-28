ac6c5da4d74a6018f725a6fa17d445a8ca3f0171
josaguil <josaguil@vector.com.mx>
2025-03-28 09:58:32 -0600

    trabajando en grafica rendimientos

--- test.py
+++ test.py
@@ -514,7 +514,7 @@ fondo = st.selectbox(
     tuple(rendimientos_fondos_df["Fondo"].unique())
 )
 
-st.write(precios_bmks_df[fondo2benchmark[fondo]])
+st.write(precios_bmks_df[fondo2benchmark[fondo]["Benchmarks"]])
 
 st.write(precios_fondos_df[fondo])
 
