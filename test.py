e47a2b403d155f3256c6bfe95df7b3f0cd0035a7
josaguil <josaguil@vector.com.mx>
2025-04-10 12:11:07 -0600

    trabajando en gráfica rendimientos

--- test.py
+++ test.py
@@ -520,8 +520,9 @@ fondo = st.selectbox(
 precios_fondo_bmks_df = pd.merge(precios_fondos_df[fondo].reset_index(), precios_bmks_df[fondo2benchmark[fondo]["Benchmarks"]].reset_index(), on="Fecha")
 precios_fondo_bmks_df[fondo2benchmark[fondo]["Benchmarks"]] = precios_fondo_bmks_df[fondo2benchmark[fondo]["Benchmarks"]].shift(1)
 precios_fondo_bmks_df.set_index("Fecha", inplace=True)
-rendimientos_fondo_bmks_df = precios_fondo_bmks_df.reset_index()
-rendimientos_fondo_bmks_df[[fondo] + fondo2benchmark[fondo]["Benchmarks"]] = rendimientos_fondo_bmks_df[[fondo] + fondo2benchmark[fondo]["Benchmarks"]].pct_change()
+# rendimientos_fondo_bmks_df = precios_fondo_bmks_df.reset_index()
+# rendimientos_fondo_bmks_df[[fondo] + fondo2benchmark[fondo]["Benchmarks"]] = rendimientos_fondo_bmks_df[[fondo] + fondo2benchmark[fondo]["Benchmarks"]].pct_change()
+rendimientos_fondo_bmks_df = precios_fondo_bmks_df.pct_change()
 rendimientos_fondo_bmks_df["BMK"] = (rendimientos_fondo_bmks_df[fondo2benchmark[fondo]["Benchmarks"]] * fondo2benchmark[fondo]["Pesos"]).sum(axis=1)
 rendimientos_fondo_bmks_df.dropna(inplace=True)
 rendimientos_fondo_bmks_df = rendimientos_fondo_bmks_df[[fondo, "BMK"]]
