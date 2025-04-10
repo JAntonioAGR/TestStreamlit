df10c350b88c70bb4130f27138896ec36af919b6
josaguil <josaguil@vector.com.mx>
2025-04-10 11:48:22 -0600

    trabajando en gráfica rendimientos

--- test.py
+++ test.py
@@ -519,7 +519,7 @@ fondo = st.selectbox(
 
 st.write(precios_fondos_df)
 st.write(precios_bmks_df)
-precios_fondo_bmks_df = pd.merge(precios_fondos_df[fondo].reset_index(), precios_bmks_df[fondo2benchmark[fondo]["Benchmarks"]].reset_index(), on="Fecha", how="left")
+precios_fondo_bmks_df = pd.merge(precios_fondos_df[fondo].reset_index(), precios_bmks_df[fondo2benchmark[fondo]["Benchmarks"]].reset_index(), on="Fecha")
 precios_fondo_bmks_df[fondo2benchmark[fondo]["Benchmarks"]] = precios_fondo_bmks_df[fondo2benchmark[fondo]["Benchmarks"]].shift(1)
 precios_fondo_bmks_df.set_index("Fecha", inplace=True)
 st.write(precios_fondo_bmks_df)
