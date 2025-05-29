bfad9c9dee9342f4804b224333c577fa1e7b2053
josaguil <josaguil@vector.com.mx>
2025-05-29 09:49:25 -0600

    test

--- test.py
+++ test.py
@@ -775,10 +775,7 @@ temp_precios_fondo_bmks_df = pd.concat([temp_precios_fondo_bmks_df1, temp_precio
 temp_precios_fondo_bmks_df.rename(columns={"Precio":fondo}, inplace=True)
 temp_precios_fondo_bmks_df = temp_precios_fondo_bmks_df[["Fecha", fondo]]
 
-st.write(temp_precios_fondo_bmks_df)
-st.write(precios_fondos_df[fondo].reset_index())
-
-precios_fondo_bmks_df = pd.merge(precios_fondos_df[fondo].reset_index(), precios_bmks_df[fondo2benchmark[fondo]["Benchmarks"]].reset_index(), on="Fecha")
+precios_fondo_bmks_df = pd.merge(temp_precios_fondo_bmks_df, precios_bmks_df[fondo2benchmark[fondo]["Benchmarks"]].reset_index(), on="Fecha")
 precios_fondo_bmks_df[fondo2benchmark[fondo]["Benchmarks"]] = precios_fondo_bmks_df[fondo2benchmark[fondo]["Benchmarks"]].shift(1)
 precios_fondo_bmks_df.set_index("Fecha", inplace=True)
 precios_fondo_bmks_df.dropna(inplace=True)
