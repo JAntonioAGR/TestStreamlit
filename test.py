36af6ae1c1825f9e45cc29be004b48b9936bc745
josaguil <josaguil@vector.com.mx>
2025-05-29 12:19:36 -0600

    test

--- test.py
+++ test.py
@@ -755,7 +755,7 @@ st.header("Rendimientos Dólares")
 
 precios_bmks_usd_df = precios_bmks_df.copy()
 precios_bmks_usd_df = precios_bmks_usd_df.div(precios_bmks_usd_df["Spot"], axis=0)
-st.write(precios_bmks_df["Spot"].shift(1))
+st.write(precios_bmks_df["Spot"])
 
 st.header("Rendimientos Históricos VS Benchmark")
 
